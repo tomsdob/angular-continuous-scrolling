51513b6bf6ef5009204581a8cfa2137f1f7f6273
Toms Dobrovoļskis <tomsdob@gmail.com>
2022-06-10 12:32:16 +0300

    IntersectionObserver bug with previous elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import {
   QueryList,
   ViewChildren,
 } from '@angular/core';
-import { ImageService } from './services/image.service';
+import { ApiService } from './services/api.service';
 import { ImageComponent } from './components/image/image.component';
 
 // Types
 import { Image } from './types/Image';
 import { Character } from './types/Character';
+import { APIData } from './types/APIData';
 
 @Component({
   selector: 'app-root',
@@ -19,24 +20,31 @@ import { Character } from './types/Character';
 })
 export class AppComponent implements OnInit, AfterViewInit {
   isLoading: boolean = false;
-  pageQueryParameter: number = 1;
+  page: number = 1;
   images: Image[] = [];
-  // TODO:
+  // TODO: assign the value before ngOnInit, as I don't know how to do it yet
+  pageAmount: number | null = null;
   lastImageObserver!: IntersectionObserver;
 
+  // Get all of the ImageComponents as elements for the IntersectionObserver
   @ViewChildren(ImageComponent, { read: ElementRef })
-  lastImage!: QueryList<Element>;
+  imageComponents!: QueryList<Element>;
 
-  constructor(private imageService: ImageService) {}
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.getImages(this.pageQueryParameter);
+    this.getPageAmount();
+    this.getImages(this.page);
     this.intersectionObserver();
   }
 
   ngAfterViewInit(): void {
-    this.lastImage.changes.subscribe(
+    this.imageComponents.changes.subscribe(
       (queryList: QueryList<ElementRef<Element>>) => {
+        // Disconnect the observer prior to observing, so it doesn't observe the
+        // previous last element/-s too
+        this.lastImageObserver.disconnect();
+
         if (queryList.last) {
           this.lastImageObserver.observe(queryList.last.nativeElement);
         }
@@ -44,23 +52,34 @@ export class AppComponent implements OnInit, AfterViewInit {
     );
   }
 
-  getImages(page: number): void {
-    this.imageService.getImages(page).subscribe((apiData) => {
-      this.isLoading = true;
+  getPageAmount(): void {
+    this.apiService
+      .getData()
+      .subscribe((data: APIData) => (this.pageAmount = data.info.pages));
+  }
 
-      apiData.results.forEach((character: Character) => {
+  getImages(page: number): void {
+    // Checking if it's the last page
+    if (this.pageAmount && this.pageAmount <= this.page - 1) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.apiService.getData(page).subscribe((data: APIData) => {
+      data.results.forEach((character: Character) => {
         const image: Image = {
           title: character.name,
           src: character.image,
         };
 
+        // Add each image to the images array
         this.images.push(image);
       });
-
-      setTimeout(() => {
-        this.isLoading = false;
-      }, 2000);
     });
+
+    this.page += 1;
+    this.isLoading = false;
   }
 
   intersectionObserver(): void {
@@ -72,10 +91,9 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     this.lastImageObserver = new IntersectionObserver((entries) => {
       entries.forEach(() => {
-        // TODO:
+        // If the element is in view, fetch new images
         if (entries[0].isIntersecting) {
-          this.pageQueryParameter += 1;
-          this.getImages(this.pageQueryParameter);
+          this.getImages(this.page);
         }
       });
     }, options);
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,41 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
+
+// Types
+import { APIData } from '../types/APIData';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private apiURL: string = 'https://rickandmortyapi.com/api/character';
+
+  constructor(private http: HttpClient) {}
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error(`An error has occured: ${error.error}`);
+    } else {
+      console.error(
+        `Back-end returned a status code of ${error.status}, body: ${error.error}`
+      );
+    }
+
+    return throwError(
+      () => new Error('An error occured, please try again later.')
+    );
+  }
+
+  getData(page?: number): Observable<APIData> {
+    const options = page ? { params: new HttpParams().set('page', page) } : {};
+
+    return this.http
+      .get<APIData>(this.apiURL, options)
+      .pipe(catchError(this.handleError));
+  }
+}
